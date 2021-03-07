e3a5b9bfe14bcf6b967f1c368bcd0f04eaee5913
Charles Lai <charlesangeloplai@gmail.com>
2021-03-07 18:22:07 -0500

    commit tree 12159144592b56a43c918ffc3fa6f56ac5aeebf5

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Your code here
+function createEmployeeRecord(record) {
+  return {
+    firstName: record[0],
+    familyName: record[1],
+    title: record[2],
+    payPerHour: record[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+}
+
+function creatEmployeeRecords(records) {
+  return records.map(function (record) {
+    return createEmployeeRecord(record);
+  });
+}
+
+function createeTimeInEvent(timeStamp) {
+  let [date, hour] = timeStamp.split(" ");
+
+  this.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date,
+  });
+
+  return this;
+}
+
+function createTimeOutEvent(timeStamp) {
+  let [date, hour] = timeStamp.split(" ");
+
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date,
+  });
+
+  return this;
+}
+
+function hoursWorkedOnDate(date) {
+  let inEvent = this.timeInEvents.find(function (e) {
+    return e.date === date;
+  });
+
+  let outEvent = this.timeOutEvents.find(function (e) {
+    return e.date === date;
+  });
+
+  return outEvent.hour - inEvent.hour / 100;
+}
